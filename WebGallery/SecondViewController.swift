6d3051bf10e53af02e119465803bdc4512c105b0
Ekaterina Ryzhova <rubicante@icloud.com>
2019-02-14 11:56:15 +0300

    Initial Commit

---
+++ WebGallery/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  WebGallery
+//
+//  Created by kathelyss on 14/02/2019.
+//  Copyright © 2019 Екатерина Рыжова. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
