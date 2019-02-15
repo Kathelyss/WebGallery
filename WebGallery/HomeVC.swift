55d93c620b04e2df2fe48f99f8c3c73542794327
Ekaterina Ryzhova <rubicante@icloud.com>
2019-02-16 00:51:20 +0300

    >

---
+++ WebGallery/HomeVC.swift
@@ -0,0 +1,16 @@
+//
+//  HomeVC.swift
+//  WebGallery
+//
+//  Created by kathelyss on 14/02/2019.
+//  Copyright © 2019 Екатерина Рыжова. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
